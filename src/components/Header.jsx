272c97f0689459859209afdf697c321d60fe492f
Vishnu Prakash <148459380+Vishnuprakashv@users.noreply.github.com>
2025-03-08 22:52:54 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ import Typed from 'typed.js';
 import style from "./header.module.css";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import CV from "src/components/My_Document/pdf/Vishnu Prakash(Resume).pdf";
 import CV from "./My_Document/pdf/Vishnu_Prakash.pdf";
 import ME from "./My_Document/image/IMG_20230502_090629_922-removebg-preview.png";
 
